dbd0c6e1d4009cbb6961f40305d7361e3e9c7ff5
wlsdpcks <80802907+wlsdpcks@users.noreply.github.com>
2022-10-30 16:56:19 +0900

    22. 이벤트 처리하기

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  function goalInputHandler(enteredText){
+    setEnteredGoalText(enteredText);
+  };
+
+  function addGoalHandler(){
+    console.log(enteredGoalText);
+  };
+
   return (
     <View style={styles.appContainer}>
 
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!'/>
-        <Button title='Add Goal'/>
+        <TextInput style={styles.textInput} placeholder='Your course goal!' onChangeText={goalInputHandler}/>
+        <Button title='Add Goal' onPress={addGoalHandler}/>
       </View>
 
       <View style={styles.goalsContainer}>
