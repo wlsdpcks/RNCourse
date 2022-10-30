f6a80cb4d1d00237b6ebc34b46db59fe9f0beeb4
wlsdpcks <80802907+wlsdpcks@users.noreply.github.com>
2022-10-30 16:46:51 +0900

    21. 레이아웃 개선하기

--- App.js
+++ App.js
@@ -3,30 +3,45 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appContainer}>
+
       <View style={styles.inputContainer}>
         <TextInput style={styles.textInput} placeholder='Your course goal!'/>
         <Button title='Add Goal'/>
       </View>
-      <View>
+
+      <View style={styles.goalsContainer}>
         <Text>List of Goal..</Text>
       </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer:{
+    flex:1,
     padding:50,
+    paddingHorizontal:16,
+
   },
   inputContainer:{
+    flex:1,
     flexDirection:'row',
-    justifyContent:'space-between'
+    justifyContent:'space-between',
+    alignItems:'center',
+    marginBottom:24,
+    borderBottomWidth:1,
+    borderBottomColor:'#cccccc'
   },
   textInput:{
     borderWidth:1,
     borderColor:'#cccccc',
-    width:'80%',
-    marginRight:8
+    width:'70%',
+    marginRight:8,
+    padding:8,
+  },
+  goalsContainer:{
+    flex:3,
   }
   
 });
