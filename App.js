d88bd4fad9b85b43330b158c1bbb7f0f073cc21f
wlsdpcks <80802907+wlsdpcks@users.noreply.github.com>
2022-10-31 17:55:15 +0900

    30. Pressable 컴포넌트로 누르는 이벤트 처리하기

--- App.js
+++ App.js
@@ -13,6 +13,10 @@ export default function App() {
     ]);
   }
 
+  function deleteGoalHandler() {
+    console.log("DELETE");
+  }
+
   return (
     <View style={styles.appContainer}>
       <GoalInput onAddGoal={addGoalHandler} />
@@ -20,7 +24,12 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return <GoalItem text={itemData.item.text} />;
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
           }}
           keyExtractor={(item, index) => {
             return item.id;
